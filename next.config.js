a165bb758b3584e0e98b73e5ed8df02415a2e98f
probin-johori <probinvarelaluis@gmail.com>
2024-12-27 21:25:46 +0530

    Hide certification and about from public

--- next.config.js
+++ next.config.js
@@ -21,8 +21,40 @@ const nextConfig = {
     ],
   },
   eslint: {
-    ignoreDuringBuilds: true, // Add this to temporarily bypass ESLint errors
+    ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/(about|certification)',
+        has: [
+          {
+            type: 'host',
+            value: '(?!localhost:3000).*',
+          },
+        ],
+        permanent: false,
+        destination: '/'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(about|certification)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'localhost:3000'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
